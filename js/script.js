1c71b3c3c7ffb047515b38ef253abacc5eb0361f
R4Y23N <r4y23n@gmail.com>
2012-04-02 10:00:26 +0200

    step 03

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log("it works!");
+!(function (exports) {
+
+  var selections = exports.selections = {};
+
+  console.log('Here are SELECTORS:');
+
+  var selection01 = $('div');
+  selections.selection01 = selection01;
+  console.log('selection01:', selection01);
+
+  var selection02 = $('#paper');
+  selections.selection02 = selection02;
+  console.log('selection02:', selection02);
+
+  var selection03 = $('.container');
+  selections.selection03 = selection03;
+  console.log('selection03:', selection03);
+
+  var myButton = $('#mybutton');
+
+  var greets = function (e){
+  	alert('Hello Web :)');
+  };
+
+  myButton.on('click',greets);
+
+}(this));
