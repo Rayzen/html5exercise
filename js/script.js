2af4094771834b2ade88ab57757ff8ae69d2f55e
R4Y23N <r4y23n@gmail.com>
2012-04-02 10:50:51 +0200

    feature drwCircle added

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@
   });
 
   
-  Step 04*/
+  Step 04
   var buttonWhite = $('#button-white');
   var buttonGray = $('#button-gray');
   var containers = $('.container');
@@ -47,6 +47,34 @@
   buttonGray.on('click', function (e) {
     containers.removeClass('bg-white');
     containers.addClass('bg-gray');
+  });
+
+/*Step 05*/
+  var canvas = $('#paper');
+  var ctx = canvas[0].getContext("2d");
+
+  var buttonWhite = $('#button-white');
+  var buttonGray = $('#button-gray');
+  var containers = $('.container');
+
+  var drawCircle = function (x, y, radius) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2*Math.PI, true);
+    ctx.stroke();
+  };
+
+  buttonWhite.on('click', function (e) {
+    containers.removeClass('bg-gray');
+    containers.addClass('bg-white');
+  });
+
+  buttonGray.on('click', function (e) {
+    containers.removeClass('bg-white');
+    containers.addClass('bg-gray');
+  });
+
+  canvas.on('mousedown', function (e) {
+    drawCircle(e.offsetX, e.offsetY, 16);
   });
 
 }(this));
